8b518308c124c9e519572a4b7c16d6366cdcf7a1
applesandorangelas <acheng9321@gmail.com>
2021-06-16 17:50:13 -0700

    finished primitive data type and string exercises

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -17,6 +17,27 @@ public class Exercise_04 {
 
         // write your code below
 
+        boolean beb= 1==2;
+        System.out.println("boolean beb is:" +beb);
+
+        byte b=122;
+        System.out.println("byte b is:" +b);
+
+        short s= 23123;
+        System.out.println("short s is:" +s);
+
+        long l= 1231231231l;
+        System.out.println("long l is:" +l);
+
+        char charlie= 'u';
+        System.out.println("char charlie is:" +charlie);
+
+        float f=11231231231212312312f;
+        System.out.println("float f is:" +f);
+
+        double d=1231231231112319232978978969607578678d;
+        System.out.println("double d is:" +d);
+
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,25 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int x=str.length();
+        System.out.println("length of first string is" + x);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        int y=str2.length();
+        System.out.println("length of second string is" + y);
+        boolean samelength= x==y;
+        System.out.println("are they the same lengths?" +samelength);
+
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String newstr = str+str2;
+        System.out.println(newstr);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String str3= str.replace("!","o");
+        System.out.println(str3);
 
     }
 
